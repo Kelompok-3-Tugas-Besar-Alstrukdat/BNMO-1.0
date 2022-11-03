640861e4824478a6e6cff9cbdab5a7de32d513f3
fahreza47 <18221013@std.stei.itb.ac.id>
2022-11-03 18:16:12 +0700

    Edit diner_dash .c .h

--- src/Game/Diner Dash/diner_dash.c
+++ src/Game/Diner Dash/diner_dash.c
@@ -1,39 +1,41 @@
 #include "diner_dash.h"
 
 void diner_dash(){
+    Diner_d masak, sajikan;
     int i = 0;
     printf("Selamat Datang di Diner Dash Dash!\n\n");
-    printf("Saldo: %d\n\n", saldo);
-    printf("Daftar Pesanan\n");
-    printf("Makanan\t| Durasi Memasak\t| Ketahanan\t| Harga\n");
-    printf("-----------------------------------------------------------");
-    for (i=1 ;i<=3;i++){
-        printf("M%d\t| %d\t| %d\t| %d\n", id, durasi, resist, price);
-    }
-    printf("\n");
-    printf("Daftar Makanan yang sedang dimasak\n");
-    printf("Makanan\t| Sisa durasi memasak\n");
-    printf("-------------------------------------------");
-    if (t_c > 0){
-        for (i=1;i<=t_c;i++){
-            printf("M%d\t| %d\n");
+    while ((Pelanggan < 7) && (Count < 15) ){ //Jika pelanggan lebih dari 7 atau sudah melayari 15 orang (15 persons served, then game ends)
+        printf("Saldo: %d\n\n", saldo);
+        printf("Daftar Pesanan\n");
+        printf("Makanan\t| Durasi Memasak\t| Ketahanan\t| Harga\n");
+        printf("-----------------------------------------------------------");
+        for (i=1 ;i<=3;i++){
+            printf("M%d\t| %d\t| %d\t| %d\n", Diner_d.Elmt[i-1].Mid, Diner_d.Elmt[i-1].Dur, Diner_d.Elmt[i-1].Tahan, Diner_d.Elmt[i-1].Harga);
         }
-    
-    }
-
-    printf("\n");
-    printf("Daftar Makanan yang dapat disajikan\n");
-    printf("Makanan\t| Sisa ketahanan makanan\n");
-    printf("-------------------------------------------");
-    if (served > 0){
-        for (i=1;i<=served;i++){
-            printf("M%d\t| %d\n");
+        printf("\n");
+        printf("Daftar Makanan yang sedang dimasak\n");
+        printf("Makanan\t| Sisa durasi memasak\n");
+        printf("-------------------------------------------");
+        if (t_c > 0){
+            for (i=1;i<=t_c;i++){
+                printf("M%d\t| %d\n");
+            }
+        
         }
-    
-    }
-    
-    printf("MASUKKAN COMMAND: ");
-    scanf("%s",&cmd);
-    
 
+        printf("\n");
+        printf("Daftar Makanan yang dapat disajikan\n");
+        printf("Makanan\t| Sisa ketahanan makanan\n");
+        printf("-------------------------------------------");
+        if (served > 0){
+            for (i=1;i<=served;i++){
+                printf("M%d\t| %d\n");
+            }
+        
+        }
+        
+        printf("MASUKKAN COMMAND: ");
+        scanf("%s",&cmd);
+        
+    }
 }
--- src/Game/Diner Dash/diner_dash.h
+++ src/Game/Diner Dash/diner_dash.h
@@ -3,6 +3,30 @@
 #include <math.h>
 
 
+typedef int keytype;
+typedef int valuetype;
+typedef int address;
+typedef int Price;
+typedef int Resist;
+typedef int Duration;
+typedef int Id;
+typedef int Count;
+
+typedef struct
+{
+	Id Mid;
+	Duration Dur;
+    Resist Tahan;
+    Price Harga; 
+
+} infotype;
+
+typedef struct
+{
+	infotype Elmt[10];
+    Count Jml;
+} Diner_d;
+
 #ifndef Diner_Dash
 #define Diner_Dash
 
