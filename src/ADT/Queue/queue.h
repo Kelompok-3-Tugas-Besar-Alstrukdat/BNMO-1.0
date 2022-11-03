f57547edbe7993f294ec4b98d9c73f8c421da329
WildhanH <16521487@mahasiswa.itb.ac.id>
2022-11-04 02:29:45 +0700

    create ADT queue with dynamic allocation

---
+++ src/ADT/Queue/queue.h
@@ -0,0 +1,70 @@
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi dinamik */
+#ifndef QUEUE
+#define QUEUE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+
+#define IDX_UNDEF -1
+#define INIT_CAPACITY 100
+
+/* Definisi elemen dan address */
+typedef int ElType;
+typedef struct {
+	ElType *buffer;
+	int idxHead;
+	int idxTail;
+    int capacity;
+} Queue;
+
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah Queue, maka akses elemen : */
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define     HEAD(q) (q).buffer[(q).idxHead]
+#define     TAIL(q) (q).buffer[(q).idxTail]
+#define CAPACITY(q) (q).capacity
+
+/* *** Kreator *** */
+void CreateQueue(Queue *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmpty(Queue q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFull(Queue q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD */
+/* IDX_HEAD selalu bernilai 0 */
+
+int length(Queue q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong */
+
+/* *** Primitif Add/Delete *** */
+void enqueue(Queue *q, ElType val);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q mungkin penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL bertambah satu */
+
+void dequeue(Queue *q, ElType *val);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., semua elemen bergeser ke kiri, dan q mungkin kosong */
+
+/* *** Display Queue *** */
+void displayQueue(Queue q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
+
+#endif
