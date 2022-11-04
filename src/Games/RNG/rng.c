09920c1a7ab9a0fc9cdc0b033391cb12539a6cce
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-04 21:41:04 +0700

    edit rng.c

--- src/Games/RNG/rng.c
+++ src/Games/RNG/rng.c
@@ -9,13 +9,14 @@ int RandomNumber()
 
 int main()
 {
-    printf("RNG telah dimulai. Uji keberuntungan Anda dengan menebak X.\n")
+    printf("RNG telah dimulai. Uji keberuntungan Anda dengan menebak X.\n");
     int X = RandomNumber(), score = 110, attempt = 0, tebakan;
     do
     {
         attempt++;
         score -= 10;
-        scanf("Tebakan: %d", &tebakan);
+        printf("Tebakan: ");
+        scanf("%d", &tebakan);
         if (X < tebakan)
         {
             printf("Lebih kecil\n");
@@ -32,8 +33,8 @@ int main()
     while ((attempt < 10) && (tebakan != X));
     if (tebakan != X)
     {
-        printf("\nMaaf, Anda kurang beruntung. Kesempatan Anda telah habis. Coba lagi lain waktu.")
+        printf("\nMaaf, Anda kurang beruntung. Kesempatan Anda telah habis. Coba lagi lain waktu.\n");
     }
-    printf("Skor Anda: %d", score);
+    printf("Skor Anda: %d\n", score);
     return 0;
 }
