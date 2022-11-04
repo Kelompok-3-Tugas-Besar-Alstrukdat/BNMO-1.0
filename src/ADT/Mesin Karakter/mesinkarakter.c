b123d31c7e854375b8c8b4efd89b76f336ea4a1f
WildhanH <16521487@mahasiswa.itb.ac.id>
2022-11-04 16:36:41 +0700

    edit ADT mesinkarakter

--- src/ADT/Mesin Karakter/mesinkarakter.c
+++ src/ADT/Mesin Karakter/mesinkarakter.c
@@ -1,19 +1,16 @@
-/* File: mesinkarakter.h */
-/* Definisi Mesin Karakter */
-#ifndef MESIN_KARAKTER
-#define MESIN_KARAKTER
-
-#include <stdio.h>
-#include "boolean.h"
-
-#define MARK '.'
-#define ENTER '\n'
+#include "mesinkarakter.h"
 
 /* State Mesin */
-extern char currentChar;
-extern boolean EOP;
+char currentChar;
+boolean EOP;
+static FILE *pita;
+static int retval;
 
-void START();
+void START(char *filename)
+{
+    pita = fopen(filename,"r");
+    ADV();
+}
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari stdin.
@@ -22,30 +19,35 @@ void START();
           Jika currentChar != MARK maka EOP akan padam (false)
           Jika currentChar = MARK maka EOP akan menyala (true) */
 
-void ADV();
+void ADV()
+{
+    retval = fscanf(pita, "%c", &currentChar);
+    EOP = (currentChar == MARK);
+    if (EOP) {
+        fclose(pita);
+    }
+}
 /* Pita dimajukan satu karakter.
    I.S. : Karakter pada jendela = currentChar, currentChar != MARK
    F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
           currentChar mungkin = MARK
           Jika  currentChar = MARK maka EOP akan menyala (true) */
 
-void COMMAND();
+void CharCOMMAND()
+{
+    pita = stdin;
+    ADV();
+}
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari stdin.
    I.S. : sembarang
    F.S. : currentChar adalah karakter pertama pada pita
-          Jika currentChar != ENTER maka EOP akan padam (false)
-          Jika currentChar = ENTER maka EOP akan menyala (true) */
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
 
-void ADVCMD();
-/* Pita dimajukan satu karakter.
-   I.S. : Karakter pada jendela = currentChar, currentChar != ENTER
-   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
-          currentChar mungkin = ENTER
-          Jika  currentChar = ENTER maka EOP akan menyala (true) */
-
-char GetCC();
+char GetCC()
+{
+    return currentChar;
+}
 /* Mengirimkan currentChar */
-
-#endif
--- src/ADT/Mesin Karakter/mesinkarakter.h
+++ src/ADT/Mesin Karakter/mesinkarakter.h
@@ -1,16 +1,18 @@
-#include "mesinkar.h"
+/* File: mesinkarakter.h */
+/* Definisi Mesin Karakter */
+#ifndef MESIN_KARAKTER
+#define MESIN_KARAKTER
+
+#include <stdio.h>
+#include "boolean.h"
+
+#define MARK '.'
 
 /* State Mesin */
-char currentChar;
-boolean EOP;
-static FILE *pita;
-static int retval;
+extern char currentChar;
+extern boolean EOP;
 
-void START(char *filename)
-{
-    pita = fopen(filename,"r");
-    ADV();
-}
+void START();
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari stdin.
@@ -19,46 +21,23 @@ void START(char *filename)
           Jika currentChar != MARK maka EOP akan padam (false)
           Jika currentChar = MARK maka EOP akan menyala (true) */
 
-void ADV()
-{
-    retval = fscanf(pita, "%c", &currentChar);
-    EOP = (currentChar == MARK);
-    if (EOP) {
-        fclose(pita);
-    }
-}
+void ADV();
 /* Pita dimajukan satu karakter.
    I.S. : Karakter pada jendela = currentChar, currentChar != MARK
    F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
           currentChar mungkin = MARK
           Jika  currentChar = MARK maka EOP akan menyala (true) */
 
-void COMMAND()
-{
-    pita = stdin;
-    ADV();
-}
+void CharCOMMAND();
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari stdin.
    I.S. : sembarang
    F.S. : currentChar adalah karakter pertama pada pita
-          Jika currentChar != ENTER maka EOP akan padam (false)
-          Jika currentChar = ENTER maka EOP akan menyala (true) */
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
 
-void ADVCMD()
-{
-    retval = fscanf(pita, "%c", &currentChar);
-    EOP = (currentChar == ENTER);
-}
-/* Pita dimajukan satu karakter.
-   I.S. : Karakter pada jendela = currentChar, currentChar != ENTER
-   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
-          currentChar mungkin = ENTER
-          Jika  currentChar = ENTER maka EOP akan menyala (true) */
-
-char GetCC()
-{
-    return currentChar;
-}
+char GetCC();
 /* Mengirimkan currentChar */
+
+#endif
