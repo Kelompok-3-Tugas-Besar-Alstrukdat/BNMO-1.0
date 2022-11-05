16def568478f4915f233dc5af9373d042af188bb
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-05 13:33:24 +0700

    create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "src/ADT/Array/arraydin.h"
+#include "src/ADT/MesinKarakter/mesinkarakter.h"
+#include "src/ADT/MesinKata/mesinkata.h"
+#include "src/ADT/Queue/queue.h"
+
