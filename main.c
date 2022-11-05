e015f5b600c529a387b9b72a8f13575afc2f3ef2
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-05 19:38:51 +0700

    create main.c

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
+//--------------------------------------------------------------- C LIBRARY ---------------------------------------------------------------//
 #include <stdio.h>
 #include <stdlib.h>
 
+//------------------------------------------------------------ FILE HEADER ADT ------------------------------------------------------------//
 #include "src/ADT/Array/arraydin.h"
 #include "src/ADT/MesinKarakter/mesinkarakter.h"
 #include "src/ADT/MesinKata/mesinkata.h"
 #include "src/ADT/Queue/queue.h"
 
+//--------------------------------------------------- FILE HEADER FUNCTION FOR COMMAND ----------------------------------------------------//
+#include "src/Command/start.h"
+#include "src/Command/load.h"
+#include "src/Command/creategame.h"
+#include "src/Command/listgame.h"
+#include "src/Command/deletegame.h"
+#include "src/Command/queuegame.h"
+#include "src/Command/playgame.h"
+#include "src/Command/skipgame.h"
+#include "src/Command/quit.h"
+#include "src/Command/help.h"
+#include "src/Command/another_command.h"
+
+void main()
+{
+    printf("ENTER COMMAND (START/LOAD): ");
+    Word INPUT;
+    COMMAND();
+    INPUT = currentWord;
+}
