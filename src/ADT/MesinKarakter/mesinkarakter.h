f3f8791074115ea1a4416f4eb8410633914ef8d6
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-06 17:04:36 +0700

    edit mesinkarakter.h

--- src/ADT/MesinKarakter/mesinkarakter.h
+++ src/ADT/MesinKarakter/mesinkarakter.h
@@ -13,7 +13,7 @@
 extern char currentChar;
 extern boolean EOP;
 
-void START();
+void START(char *filename);
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari file dokumen.
