de5ada1651058d2518151f4f0ca49ffff3048a61
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-06 19:35:35 +0700

    edit another_command.c

--- src/Command/another_command.c
+++ src/Command/another_command.c
@@ -4,85 +4,84 @@ ArrayDin validCOMMAND()
 {
     ArrayDin command = MakeArrayDin();
     command.Neff = 11;
-    char tempCMD[20];
     int i;
 
     // START
     command.Elmt[0].Length = 5;
-    tempCMD[20] = "START";
+    char CMDSTART[15] = "START";
     for (i = 0; i < 5; i++)
     {
-        command.Elmt[0].TabWord[i] = tempCMD[i];
+        command.Elmt[0].TabWord[i] = CMDSTART[i];
     }
     // LOAD
     command.Elmt[1].Length = 4;
-    tempCMD[20] = "LOAD";
+    char CMDLOAD[15] = "LOAD";
     for (i = 0; i < 4; i++)
     {
-        command.Elmt[1].TabWord[i] = tempCMD[i];
+        command.Elmt[1].TabWord[i] = CMDLOAD[i];
     }
     // SAVE
     command.Elmt[2].Length = 4;
-    tempCMD[20] = "SAVE";
+    char CMDSAVE[15] = "SAVE";
     for (i = 0; i < 4; i++)
     {
-        command.Elmt[2].TabWord[i] = tempCMD[i];
+        command.Elmt[2].TabWord[i] = CMDSAVE[i];
     }
     // CREATE GAME
     command.Elmt[3].Length = 11;
-    tempCMD[20] = "CREATE GAME";
+    char CMDCG[15] = "CREATE GAME";
     for (i = 0; i < 11; i++)
     {
-        command.Elmt[3].TabWord[i] = tempCMD[i];
+        command.Elmt[3].TabWord[i] = CMDCG[i];
     }
     // LIST GAME
     command.Elmt[4].Length = 9;
-    tempCMD[20] = "LIST GAME";
+    char CMDLG[15] = "LIST GAME";
     for (i = 0; i < 9; i++)
     {
-        command.Elmt[4].TabWord[i] = tempCMD[i];
+        command.Elmt[4].TabWord[i] = CMDLG[i];
     }
     // DELETE GAME
     command.Elmt[5].Length = 11;
-    tempCMD[20] = "DELETE GAME";
+    char CMDDG[15] = "DELETE GAME";
     for (i = 0; i < 11; i++)
     {
-        command.Elmt[5].TabWord[i] = tempCMD[i];
+        command.Elmt[5].TabWord[i] = CMDDG[i];
     }
     // QUEUE GAME
     command.Elmt[6].Length = 10;
-    tempCMD[20] = "QUEUE GAME";
+    char CMDQG[15] = "QUEUE GAME";
     for (i = 0; i < 10; i++)
     {
-        command.Elmt[6].TabWord[i] = tempCMD[i];
+        command.Elmt[6].TabWord[i] = CMDQG[i];
     }
     // PLAY GAME
     command.Elmt[7].Length = 9;
-    tempCMD[20] = "PLAY GAME";
+    char CMDPG[15] = "PLAY GAME";
     for (i = 0; i < 9; i++)
     {
-        command.Elmt[7].TabWord[i] = tempCMD[i];
+        command.Elmt[7].TabWord[i] = CMDPG[i];
     }
     // SKIP GAME
     command.Elmt[8].Length = 9;
-    tempCMD[20] = "SKIP GAME";
+    char CMDSG[15] = "SKIP GAME";
     for (i = 0; i < 9; i++)
     {
-        command.Elmt[8].TabWord[i] = tempCMD[i];
+        command.Elmt[8].TabWord[i] = CMDSG[i];
     }
     // QUIT
     command.Elmt[9].Length = 4;
-    tempCMD[20] = "QUIT";
+    char CMDQUIT[15] = "QUIT";
     for (i = 0; i < 4; i++)
     {
-        command.Elmt[9].TabWord[i] = tempCMD[i];
+        command.Elmt[9].TabWord[i] = CMDQUIT[i];
     }
     // HELP
     command.Elmt[10].Length = 4;
-    tempCMD[20] = "HELP";
+    char CMDHELP[15] = "HELP";
     for (i = 0; i < 4; i++)
     {
-        command.Elmt[10].TabWord[i] = tempCMD[i];
+        command.Elmt[10].TabWord[i] = CMDHELP[i];
     }
     return command;
 }
