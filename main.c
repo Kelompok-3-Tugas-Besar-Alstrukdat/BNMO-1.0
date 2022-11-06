d105ccf99eec18cbea52d514ca692c6614f6e95e
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-06 18:36:11 +0700

    edit main.c

--- main.c
+++ main.c
@@ -24,7 +24,96 @@
 void main()
 {
     printf("ENTER COMMAND (START/LOAD): ");
-    Word INPUT;
     COMMAND();
-    INPUT = currentWord;
+    Word INPUT;
+    ArrayDin Game;
+    if (isWordEqual(currentWord,validCOMMAND().Elmt[0]))
+    {
+        INPUT = currentWord;
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < 4; i++)
+        {
+            INPUT.TabWord[i] = currentWord.TabWord[i];
+        }
+        INPUT.Length = i;
+    }
+    while ((!isWordEqual(INPUT,validCOMMAND().Elmt[0])) && (!isWordEqual(INPUT,validCOMMAND().Elmt[1])))
+    {
+        ADVCMD();
+        INPUT = currentWord;
+    }
+    if (!isWordEqual(INPUT,validCOMMAND().Elmt[0]))
+    {
+        Game = STARTGAME();
+    }
+    else
+    {
+        char filename[50];
+        int j = 0;
+        for (int i = INPUT.Length; i < currentWord.Length; i++)
+        {
+            if (currentWord.TabWord[i] != ' ')
+            {
+                filename[j] = currentWord.TabWord[i];
+                j++;
+            }
+        }
+        Game = LOADGAME(filename);
+    }
+    while (!isWordEqual(INPUT,validCOMMAND().Elmt[8]))
+    {
+        ADVCMD();
+        INPUT = currentWord;
+
+        if (isCOMMAND(INPUT))
+        {
+            if ((isWordEqual(INPUT, validCOMMAND().Elmt[0])) || (isWordEqual(INPUT, validCOMMAND().Elmt[1])))
+            {
+                printf('BNMO sudah dijalankan.');
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[2])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[3])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[4])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[5])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[6])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[7])))
+            {
+
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[8])))
+            {
+                
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[9])))
+            {
+                printf("Anda keluar dari game BNMO.\nBye bye ...\n");
+            }
+            else if ((isWordEqual(INPUT, validCOMMAND().Elmt[9])))
+            {
+
+            }
+        }
+        else
+        {
+            printf("Command tidak dikenali, silahkan masukkan command yang valid.");
+        }
+    }
 }
