e6f05006277542ca53da7475455e41471dc0b144
nabilahamnda <104730893+nabilahamnda@users.noreply.github.com>
2022-11-07 13:08:32 +0700

    update help.c

---
+++ src/Command/help.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+void help(){
+    printf("Help");
+    printf("1. Start");
+    printf("2. Load");
+    printf("3. Create Game");
+    printf("4. List Game");
+    printf("5. Delete Game");
+    printf("6. Queue Game");
+    printf("7. Play Game");
+    printf("8. Play Game");
+    printf("9. Skip Game");
+    printf("10. Quit");
+}
