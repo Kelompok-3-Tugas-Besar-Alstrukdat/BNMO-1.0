25498d1aed6e096dc56f29e2e81dde70946a9a93
fahreza47 <18221013@std.stei.itb.ac.id>
2022-11-10 14:13:35 +0700

    edit queuegame.c

---
+++ src/Command/queuegame.c
@@ -0,0 +1,34 @@
+#include "../ADT/Array/arraydin.h"
+#include "../ADT/MesinKata/mesinkata.h"
+#include "../ADT/MesinKarakter/mesinkarakter.h"
+
+void Queue(ArrayDin *Game, ArrayDin *Q){
+    int i = 0;
+    printf("Berikut adalah daftar antrian game-mu\n");
+    if (Q->Neff == 0){
+        printf("==Game Tidak Ada==\n");
+        printf("Daftar antrian mu masih kosong, silakan tambahkan game ke antrian!\n");
+    }
+    else{
+        for (i=0;i<(Q->Neff);i++){
+            printf("%d.  ",(i+1));
+            printWord(Q->Elmt[i]);
+        }
+    }
+    printf("Berikut adalah game yang tersedia: \n");
+    for (i=0;i < (Game->Neff);i++){
+            printf("M%d.  ",(i+1));
+            printWord(Game->Elmt[i]);
+    }
+    printf("Nomor Game yang mau ditambahkan ke antrian: ");
+    COMMAND();
+    int a;
+    a = toInt(currentWord);
+    if ( (a>=0) && (a <= (Game->Neff)) ){
+        printf("Game berhasil ditambahkan ke dalam daftar antrian.\n");
+        InsertLast(Q,Game->Elmt[a-1]);
+    }
+    else{
+        print("Nomor permainan tidak valid, silakan masukkan nomor game pada list.\n");
+    }
+}
