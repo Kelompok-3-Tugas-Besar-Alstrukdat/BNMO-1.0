2a3c953429c435b90a49332878f6033fb0e71337
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-11 06:15:07 +0700

    eidt playgame.c

--- src/Command/playgame.c
+++ src/Command/playgame.c
@@ -2,25 +2,68 @@
 #include "../boolean.h"
 #include "../Games/DinerDash/diner_dash.c"
 #include "../Games/RNG/rng.c"
+#include "../Games/MagicShell/MagicShell.c"
 
 void PlayGame (Queue *q){
     ArrayDin game = MakeArrayDin();
-    game.Neff = 2;
+    game.Neff = 8;
     int i;
 
     // RNG
     game.Elmt[0].Length = 3;
-    char GMRNG[15] = "RNG";
+    char GMRNG[20] = "RNG";
     for (i = 0; i < 3; i++)
     {
         game.Elmt[0].TabWord[i] = GMRNG[i];
     }
-    // Diner Dash
+    // DINER DASH
     game.Elmt[1].Length = 10;
-    char GMDINERDASH[15] = "DINER DASH";
+    char GMDD[20] = "DINER DASH";
     for (i = 0; i < 10; i++)
     {
-        game.Elmt[1].TabWord[i] = GMDINERDASH[i];
+        game.Elmt[1].TabWord[i] = GMDD[i];
+    }
+    // DINOSAUR IN EARTH
+    game.Elmt[2].Length = 17;
+    char GMDIE[20] = "DINOSAUR IN EARTH";
+    for (i = 0; i < 17; i++)
+    {
+        game.Elmt[2].TabWord[i] = GMDIE[i];
+    }
+    // RISEWOMAN
+    game.Elmt[3].Length = 9;
+    char GMRW[20] = "RISEWOMAN";
+    for (i = 0; i < 9; i++)
+    {
+        game.Elmt[3].TabWord[i] = GMRW[i];
+    }
+    // EIFFEL TOWER
+    game.Elmt[4].Length = 12;
+    char GMET[20] = "EIFFEL TOWER";
+    for (i = 0; i < 12; i++)
+    {
+        game.Elmt[4].TabWord[i] = GMET[i];
+    }
+    // HIDE IN CARTESIAN
+    game.Elmt[5].Length = 17;
+    char GMHIC[20] = "HIDE IN CARTESIAN";
+    for (i = 0; i < 17; i++)
+    {
+        game.Elmt[5].TabWord[i] = GMHIC[i];
+    }
+    // NUZZLE
+    game.Elmt[6].Length = 6;
+    char GMNZ[20] = "NUZZLE";
+    for (i = 0; i < 6; i++)
+    {
+        game.Elmt[6].TabWord[i] = GMNZ[i];
+    }
+    // MAGIC SHELL
+    game.Elmt[7].Length = 11;
+    char GMMS[20] = "MAGIC SHELL";
+    for (i = 0; i < 11; i++)
+    {
+        game.Elmt[7].TabWord[i] = GMMS[i];
     }
 
     if (!isEmptyQueue(*q)){
@@ -30,13 +73,28 @@ void PlayGame (Queue *q){
         else if (isWordEqual(HEAD(*q), game.Elmt[1])){
             runDinerDash();
         }
+        else if (isWordEqual(HEAD(*q), game.Elmt[2])){
+            printf("Permainan sedang dalam perbaikan.\n");
+        }
+        else if (isWordEqual(HEAD(*q), game.Elmt[3])){
+            printf("Permainan sedang dalam perbaikan.\n");
+        }
+        else if (isWordEqual(HEAD(*q), game.Elmt[4])){
+            printf("Permainan sedang dalam perbaikan.\n");
+        }
+        else if (isWordEqual(HEAD(*q), game.Elmt[5])){
+            runDinerDash();
+        }
+        else if (isWordEqual(HEAD(*q), game.Elmt[6])){
+            runDinerDash();
+        }
+        else if (isWordEqual(HEAD(*q), game.Elmt[7])){
+            magic_shell();
+        }
         else {
             printf("Skor Anda : %d\n", RandomNumber());
         }
         QueueType val;
         dequeueQ(q, &val);
-    }
-    else{
-        printf("Antrian game Anda kosong. Silahkan tambahkan game terlebih dahulu\n");
     }
 }
