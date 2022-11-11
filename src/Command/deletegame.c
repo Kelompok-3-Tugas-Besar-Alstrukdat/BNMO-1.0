de14442aaa2cc12b67a14d30fc310b3ad164d089
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-11 19:46:41 +0700

    edit deletegame.c

--- src/Command/deletegame.c
+++ src/Command/deletegame.c
@@ -3,7 +3,7 @@
 #include "../ADT/MesinKata/mesinkata.h"
 #include "../ADT/Array/arraydin.h"
 
-void DeleteGame(ArrayDin *Game)
+void DeleteGame(ArrayDin *Game, Queue *Gameq)
 {
     printf("Masukkan nomor permainan yang akan dihapus: ");
     COMMAND();
@@ -16,8 +16,25 @@ void DeleteGame(ArrayDin *Game)
 
     else if ((num > 7) && (num < (*Game).Neff))
     {
-        DeleteAt(Game, num);
-        printf("Prmainan berhasil dihapus.\n");
+        boolean check = true;
+        int i = 0;
+        while (check && (i <= IDX_TAIL(*Gameq)))
+        {
+            if (isWordEqual((*Game).Elmt[num], (*Gameq).buffer[i]))
+            {
+                check = false;
+            }
+            i++;
+        }
+        if (check)
+        {
+            DeleteAt(Game, num);
+            printf("Permainan berhasil dihapus.\n");
+        }
+        else
+        {
+            printf("Permainan yang sedang dalam antrian tidak dapat dihapus.\n");
+        }
     }
     else
     {
