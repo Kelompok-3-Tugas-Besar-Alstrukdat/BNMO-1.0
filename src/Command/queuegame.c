3c27c53a1048fd50ebf1fbe3dbcec0fbdf44db89
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2022-11-11 19:46:30 +0700

    edit queuegame.c

--- src/Command/queuegame.c
+++ src/Command/queuegame.c
@@ -4,10 +4,10 @@
 
 void QueueGame(ArrayDin *Game, Queue *Q){
     int i = 0;
-    printf("Berikut adalah daftar antrian game-mu\n");
+    printf("Berikut adalah daftar antrian permainanmu\n");
     if (lengthQueue(*Q) == 0){
-        printf("==Game Tidak Ada==\n");
-        printf("Daftar antrian mu masih kosong, silakan tambahkan game ke antrian!\n");
+        printf("\n-- Permainan Tidak Ada --\n");
+        printf("\nDaftar antrian permainanmu masih kosong, silakan tambahkan permainan terlebih dahulu!\n");
     }
     else{
         for (i = 0; i < lengthQueue(*Q); i++){
@@ -15,20 +15,20 @@ void QueueGame(ArrayDin *Game, Queue *Q){
             printWord(Q->buffer[i]);
         }
     }
-    printf("Berikut adalah game yang tersedia: \n");
+    printf("\n\nBerikut adalah permainan yang tersedia: \n");
     for (i=1;i < (Game->Neff);i++){
             printf("%d.  ",i);
             printWord(Game->Elmt[i]);
     }
-    printf("Nomor Game yang mau ditambahkan ke antrian: ");
+    printf("Nomor permainan yang mau ditambahkan ke antrian: ");
     COMMAND();
     int a;
     a = toInt(currentWord);
     if ( (a>0) && (a < (Game->Neff)) ){
-        printf("Game berhasil ditambahkan ke dalam daftar antrian.\n");
+        printf("Permainan berhasil ditambahkan ke dalam daftar antrian.\n");
         enqueueQ(Q,Game->Elmt[a]);
     }
     else{
-        printf("Nomor permainan tidak valid, silakan masukkan nomor game pada list.\n");
+        printf("Nomor permainan tidak valid, silakan masukkan nomor permainan yang ada pada list.\n");
     }
 }
