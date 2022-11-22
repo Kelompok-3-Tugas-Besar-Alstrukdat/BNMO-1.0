816781c708909b96beadf656b5eba4aecc4ce7d4
fahreza47 <18221013@std.stei.itb.ac.id>
2022-11-22 16:19:49 +0700

    add ADT stack

---
+++ src/ADT/Stack/Stack.h
@@ -0,0 +1,14 @@
+/* Definisi ADT _STACK dengan representasi array secara eksplisit dan alokasi dinamik */
+#ifndef _STACK
+#define _STACK
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../boolean.h"
+#include "../MesinKata/mesinkata.h"
+
+#define IDX_UNDEF -1
+#define INIT_CAPACITY 50
+
+
+#endif
